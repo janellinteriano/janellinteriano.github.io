96758c115a4ed11b5101c9b2718031685896b3c0
janellinteriano <111891542+janellinteriano@users.noreply.github.com>
2023-01-02 20:49:20 -0500

    Added chnages to portfolio website

--- js_1.js
+++ js_1.js
@@ -1,17 +1,29 @@
-// These functions open and close the contact form
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
 }
 
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
 }
 
-// This code will create close the contact form when the user clicks off of it
-// The first step is to add an event listener for any clicks on the website
-document.addEventListener("click", function(event){
-    // Here we state that if the click happens on the cancel button OR anywhere that is not the contact form AND the click does not happen on any element with the contact class then call the closeForm() function
-    if (event.target.matches(".cancel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")){
-        closeForm()
-    }
-}, false )
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "235px";
+    document.getElementById("main").style.marginLeft = "235px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
